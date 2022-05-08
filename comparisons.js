a86be6adecd3a48a8d317eb37e7823960a91e21c
Samkiroko <kirokodev@gmail.com>
2022-05-08 16:23:57 +0300

    Comparison

--- comparisons.js
+++ comparisons.js
@@ -14,5 +14,21 @@ console.log(2 != 1) // true
 // String comparison
 
 /***
- *
+ *To see whether a string is greater than another, js uses dictionary or lexicographical order
  * */
+console.log('Z' > 'A') // true
+console.log('Glow' > 'Glee') // true
+console.log('Bee' > 'Be') // true
+
+// Comparison of different types
+
+/**
+ * when comparing values of different type, javascript convert the value to number
+ *
+ * **/
+
+console.log('2' > 1) // true, string '2' becomes a number 2
+
+// ? for boolean true becomes 1 and false become 0
+console.log(true == 1) // true
+console.log(false == 0) // true
